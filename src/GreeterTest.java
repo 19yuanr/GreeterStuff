96d4b72fe4c15f2c8739340ef25347ac10400473
Reena <19yuanr@elmbrookstudents.org>
2018-01-30 10:39:06 -0600

    1D array and displayed states

--- src/GreeterTest.java
+++ src/GreeterTest.java
@@ -5,6 +5,11 @@ public class GreeterTest {
 		// TODO Auto-generated method stub
 		Greeter bob = new Greeter();
 		System.out.println(bob.toString());
+		Greeter[] joe = new Greeter[3];
+		System.out.println(joe[0].toString());
+		System.out.println(joe[1].toString());
+		System.out.println(joe[2].toString());
+		System.out.println(joe[3].toString());
 
 	}
 
